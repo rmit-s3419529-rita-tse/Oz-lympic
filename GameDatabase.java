61f175b1cb2f32acb468eb2531af67a6545c12a6
cindy9328 <s3588016@student.rmit.edu.au>
2017-05-11 03:22:07 +1000

    modify GameDatabase class
    set up the connection.still work on insert part

--- GameDatabase.java
+++ GameDatabase.java
@@ -4,44 +4,49 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collection;
+
 import org.hsqldb.Server;
 
 /**
  * The is the database connection of Ozlympic Game Program
  * which can connect to the database
- * not finish yet,just try the code from tutorial 
+ * not finish yet,just connect first,still need insert some data
  *
  * @author  SZUYING CHEN
- * @version 1.0
+ * @version 2.0
  * @since   2017-05-08
  */
 	
+	
+
 	public class GameDatabase {
-	public static void main(String[] args) {
+
+		Connection connection = null;
+		ResultSet rs = null;
+	public GameDatabase() {
 	Server hsqlServer = null;
-	Connection connection = null;
-	ResultSet rs = null;
+
+	
 	hsqlServer = new Server();
 	hsqlServer.setLogWriter(null);
 	hsqlServer.setSilent(true);
 	hsqlServer.setDatabaseName(0, "TestDB");
-	hsqlServer.setDatabasePath(0, "file:MYDB");
+	hsqlServer.setDatabasePath(0, "file:MyDB");
 	hsqlServer.start();
 	// making a connection
 	try {
 	Class.forName("org.hsqldb.jdbcDriver");
-	connection =
-	DriverManager.getConnection("jdbc:hsqldb:TestDB", "sa", "123");
-	connection.prepareStatement("drop table barcodes if exists;").execute();
-	connection.prepareStatement("create table barcodes (id integer, barcode varchar(20) not null);").execute();
-	connection.prepareStatement("insert into barcodes (id, barcode)"
-	+ "values (1, '12345577');").execute();
-	//
-	// // query from the db
-	rs = connection.prepareStatement("select id, barcode from barcodes;").executeQuery();
-	rs.next();
-	System.out.println(String.format("ID: %1d, Name: %1s", rs.getInt(1), rs.getString(2)));
-	connection.commit();
+	connection =DriverManager.getConnection("jdbc:hsqldb:file:TestDB", "sa", "123");
+	//The table can only be created but not dropped, so once it created, comment out the following commands
+	//connection.prepareStatement("drop table barcodes ifexists;").execute();
+	try{
+		connection.prepareStatement("create table participants(id varchar(20), type varchar(20), name varchar(20), age integer, state varchar(20) );").execute();
+	}catch(Exception e2){
+		
+	}
+
+
 	} catch (SQLException e2) {
 	e2.printStackTrace();
 	} catch (ClassNotFoundException e2) {
@@ -50,76 +55,23 @@ import org.hsqldb.Server;
 	// end of stub code for in/out stub
 	}
 	
-public static void createTable(String[] args) {
-		
-		Connection con = null;
-		Statement stmt = null;
-		int result = 0;
-		
-		try {
-			Class.forName("org.hsqldb.jdbc.JDBCDriver");
-			con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/testdb", "SA", "");
-			stmt = con.createStatement();
+	public void AddParticipants(String ID, String strType, String strName, Integer Age, String State){
+		try{
+//			connection.prepareStatement("insert into participants (id, type, name, age, state) "+
+//				"values ('Oz1123', 'officer', 'Derek', 21,'WA'), " +
+//				"('Oz3434', 'sprinter', 'Mary', 35, 'VIC'), "+
+//				"('Oz0091', 'super', 'Hannah', 24, 'NSW'), "+
+//				"('Oz1234', 'swimmer', 'Beck', 30, 'TAS');").execute();
 			
-			result = stmt.executeUpdate("CREATE TABLE tutorials_tbl                                       (id INT NOT NULL, title VARCHAR(50) NOT NULL,author VARCHAR(20) NOT NULL,                                           submission_date DATE,PRIMARY KEY (id));");
-			//Warning, your compiler might not like the newlines in the SQL query
-	
-		}  catch (Exception e) {
-			e.printStackTrace(System.out);
-		}
-		System.out.println("Table created successfully");
-	}
-
-
-
-	   public static void viewData(String[] args) {
-	      Connection con = null;
-	      Statement stmt = null;
-	      ResultSet result = null;
-	      
-	      try {
-	         Class.forName("org.hsqldb.jdbc.JDBCDriver");
-	         con = DriverManager.getConnection(
-	            "jdbc:hsqldb:hsql://localhost/testdb", "SA", "");
-	         stmt = con.createStatement();
-	         result = stmt.executeQuery(
-	            "SELECT * FROM tutorials_tbl");
-	         
-	         while(result.next()){
-	            System.out.println(result.getInt("id")+" | "+
-	               result.getString("title")+" | "+
-	               result.getString("author"));
-	         }
-	      } catch (Exception e) {
-	         e.printStackTrace(System.out);
-	      }
-	   }
-	   public static void upDateData(String[] args) {
-	        Connection con = null;
-	        Statement stmt = null;
-	        ResultSet output = null;
-	        int result = 0;
+			connection.prepareStatement("insert into participants (id, type, name, age, state) "+
+			"values ("+ ID +", "+strType+", "+ strName +", "+Age+", "+State+");").execute();
 			
-	        try {
-	            Class.forName("org.hsqldb.jdbc.JDBCDriver");
-	            con = DriverManager.getConnection(
-					"jdbc:hsqldb:hsql://localhost/testdb", "SA", "");
-	            stmt = con.createStatement();
-	            result = stmt.executeUpdate(
-					"UPDATE tutorials_tbl SET title = 'Data Structures' WHERE id = 100");
-					
-	            output = stmt.executeQuery(
-					"SELECT * FROM tutorials_tbl");
-					
-	            while(output.next()){
-	                System.out.println(output.getInt("id")+" | "+
-				     		output.getString("title")+" | "+
-							output.getString("author"));
-	                }		
-	            } catch (Exception e) {
-	                e.printStackTrace(System.out);
-	        }
-	        System.out.println(result+" Rows effected");
-	    }
+	//		connection.prepareStatement(strSQL).execute();
+			
+			connection.commit();
+		} catch (SQLException e2) {
+			e2.printStackTrace();
+			}
 	}
+	}	
 	
